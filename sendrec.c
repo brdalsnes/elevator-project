2ab0ddab6f4c84d6a506883aec0ce1640ce078ba
brdalsnes <bjornar.dalsnes@gmail.com>
2016-04-13 13:01:28 +0200

    Update sendrec.c

--- sendrec.c
+++ sendrec.c
@@ -1,4 +1,3 @@
-
 #include "sendrec.h"
 
 void die(char *s)
@@ -11,8 +10,7 @@ void* Sender()
 {
     struct sockaddr_in si_me, si_other;
      
-    int s, recv_len;
-  // 	socklen_t slen = sizeof(si_other);
+    int s;
     char moskus[BUFLEN];
     strcat(moskus, "Hello Moskus");
     //create a UDP socket
@@ -21,29 +19,26 @@ void* Sender()
         die("socket");
     }
      
-    // zero out the structure
+    // zero out the structur
     memset((char *) &si_me, 0, sizeof(si_me));
+    memset((char *) &si_other, 0, sizeof(si_other));
+    
+    
     si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(PORT);
     inet_pton(AF_INET, SADDRESS, &si_other.sin_addr);
     si_me.sin_family = AF_INET;
     si_me.sin_port = htons(PORT);
     inet_pton(AF_INET, OURADDRESS, &si_me.sin_addr);
-    //si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-    //bind socket to port
-    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
-    {
-        die("bind");
-    }
-   
+    
     while(1)
     {
-    	
+        
 
-        if (sendto(s, moskus, BUFLEN, 0, (struct sockaddr*) &si_other, sizeof(si_other)) == -1)
+        if (sendto(s, moskus, BUFLEN, 0, (struct sockaddr*) &si_me, sizeof(si_me)) == -1)
         {
             die("sendto()");
-       	
+        
         }
         printf("Sendt packet to %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
         printf("Data from sender: %s\n" , moskus);    
@@ -59,27 +54,29 @@ void* Reciever()
 {
     struct sockaddr_in si_me, si_other;
      
-    int r, recv_len, slen = sizeof(si_other);
-   // socklen_t restrict slen = sizeof(si_other);
+    int s, recv_len;
+    socklen_t slen = sizeof(si_other);
     char buf[BUFLEN];
     
     //create a UDP socket
-    if ((r=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
     {
         die("socket");
     }
      
     // zero out the structure
     memset((char *) &si_me, 0, sizeof(si_me));
+    memset((char *) &si_other, 0, sizeof(si_other));
+    
     si_other.sin_family = AF_INET;
+    si_other.sin_port = PORT;
     inet_pton(AF_INET, SADDRESS, &si_other.sin_addr);
     si_me.sin_family = AF_INET;
     si_me.sin_port = htons(PORT);
     inet_pton(AF_INET, OURADDRESS, &si_me.sin_addr);
-    //si_me.sin_addr.s_addr = htonl(INADDR_ANY);
      
     //bind socket to port
-    if( bind(r , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
+    if( bind(s, (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
     {
         die("bind");
     }
@@ -91,7 +88,7 @@ void* Reciever()
       
         memset(&buf[0], 0, sizeof(buf)); 
         //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(r, buf, BUFLEN, 0, &si_other, &slen)) == -1)
+        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, &si_me, &slen)) == -1)
         {
             die("recvfrom()");
         }
@@ -101,6 +98,6 @@ void* Reciever()
         printf("Data received: %s\n" , buf);    
     }
  
-    close(r);
+    close(s);
     return 0;
 }
